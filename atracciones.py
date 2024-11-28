8ec97811e0f5c33debf57dea7ce4ab93ae7ad173
kiqueLS <kique_lopez_@hotmail.com>
2024-11-28 12:15:44 -0600

    "E4: Fin del examen"

--- atracciones.py
+++ atracciones.py
@@ -1,2 +1,3 @@
 atracciones_modernas = ['Simulador Virtual', 'Carrusel Moderno']
 atracciones_antiguas = ['Rueda de la Fortuna', 'Caballitos']
+atracciones = ['Carros chocones', 'Barco pirata', 'Sillas voladoras']
